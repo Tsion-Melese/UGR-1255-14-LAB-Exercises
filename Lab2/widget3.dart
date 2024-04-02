e0722050e8bf4c0597a06f6874f692924de25e42
Tsion-Melese <150340883+Tsion-Melese@users.noreply.github.com>
2024-04-02 16:59:02 +0300

    Update widget3.dart

--- Lab2/widget3.dart
+++ Lab2/widget3.dart
@@ -39,157 +39,117 @@ class CarPage extends StatelessWidget {
           ],
           centerTitle: true,
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 200,
-              child: Image.asset('assests/download (1).jpg'),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                '1975 Porsche 911 Carrera',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 200,
+                child: Image.asset('assets/download (1).jpg'),
               ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.thumb_up),
-                  onPressed: () {},
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  '1975 Porsche 911 Carrera',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
-                Text("0"),
-                IconButton(
-                  icon: Icon(Icons.mode_comment),
-                  onPressed: () {},
-                ),
-                Text("0"),
-                IconButton(
-                  icon: Icon(Icons.share),
-                  onPressed: () {},
-                ),
-                Text("Share")
-              ],
-            ),
-            SizedBox(height: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text(
-                    'Essential information',
-                    style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Text(
-                    '1 of 3 done',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16),
-            Container(
-              color: Colors.black,
-              height: 1,
-            ),
-            SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.check_circle,
-                        color: Colors.green,
-                      ),
-                      SizedBox(width: 8),
-                      Text(
+                  IconButton(
+                    icon: Icon(Icons.thumb_up),
+                    onPressed: () {},
+                  ),
+                  Text("0"),
+                  IconButton(
+                    icon: Icon(Icons.mode_comment),
+                    onPressed: () {},
+                  ),
+                  Text("0"),
+                  IconButton(
+                    icon: Icon(Icons.share),
+                    onPressed: () {},
+                  ),
+                  Text("Share")
+                ],
+              ),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      'Essential information',
+                      style:
+                          TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      '1 of 3 done',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Container(
+                color: Colors.black,
+                height: 1,
+              ),
+              SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.check_circle, color: Colors.green),
+                      title: Text(
                         'Year, Make, Model, VIN',
                         style: TextStyle(
                             fontSize: 22, fontWeight: FontWeight.bold),
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Year: 1997',
-                    style: TextStyle(fontSize: 17, color: Colors.grey[800]),
-                  ),
-                  Text(
-                    'Make:Porsche',
-                    style: TextStyle(fontSize: 17, color: Colors.grey[800]),
-                  ),
-                  Text(
-                    'Model: 911 Carrera',
-                    style: TextStyle(fontSize: 17, color: Colors.grey[800]),
-                  ),
-                  Text(
-                    'VIN: 9115400029',
-                    style: TextStyle(fontSize: 17, color: Colors.grey[800]),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    color: Colors.grey[800],
-                    height: 1,
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Description',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              color: Colors.grey[800],
-              height: 1,
-            ),
-            SizedBox(height: 8),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Photo',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
                     ),
-                  ),
-                  Icon(
-                    Icons.edit,
-                    color: Colors.black,
-                  ),
-                ],
+                    ListTile(
+                      title: Text('Year: 1997'),
+                    ),
+                    ListTile(
+                      title: Text('Make: Porsche'),
+                    ),
+                    ListTile(
+                      title: Text('Model: 911 Carrera'),
+                    ),
+                    ListTile(
+                      title: Text('VIN: 9115400029'),
+                    ),
+                    Divider(color: Colors.grey),
+                    ListTile(
+                      leading: Icon(Icons.done),
+                      title: Text(
+                        'Description',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(Icons.edit),
+                    ),
+                    Divider(color: Colors.grey),
+                    ListTile(
+                      leading: Icon(Icons.done),
+                      title: Text(
+                        'Photos',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(Icons.edit),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              color: Colors.grey[800],
-              height: 1,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
